1f81c65bbc3d1f56ce2676bf72fe3db643cd6607
Mateo Acosta <twishm08@gmail.com>
2025-05-29 13:20:12 -0500

    Update caraBMO.py

--- pages/caraBMO.py
+++ pages/caraBMO.py
@@ -1,1 +1,43 @@
+import paho.mqtt.client as paho
+import time
+import streamlit as st
+import json
+import platform
 
+# Muestra la versión de Python junto con detalles adicionales
+st.write("Versión de Python:", platform.python_version())
+
+values = 0.0
+act1="OFF"
+
+def on_publish(client,userdata,result):             #create function for callback
+    print("el dato ha sido publicado \n")
+    pass
+
+def on_message(client, userdata, message):
+    global message_received
+    time.sleep(2)
+    message_received=str(message.payload.decode("utf-8"))
+    st.write(message_received)
+
+
+broker="broker.mqttdashboard.com"
+port=1883
+client1= paho.Client("MateoA")
+client1.on_message = on_message
+
+
+
+st.title("SALUDA A BMO")
+   
+if st.button('SALUDAR'):
+    act1="DESPERTARSE"
+    client1= paho.Client("MateoA")                           
+    client1.on_publish = on_publish                          
+    client1.connect(broker,port)  
+    message =json.dumps({"Act1":act1})
+    ret= client1.publish("BMO", message)
+    
+ 
+else:
+    st.write('')
